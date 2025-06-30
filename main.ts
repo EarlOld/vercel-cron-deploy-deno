aece9e8521f7165029a9a51c5ad0b862857a45c5
Сапожник Дмитро <dimonsuper010@gmail.com>
2025-06-30 23:18:35 +0300

    Refactor cron job timing and structure in main.ts; update task commands in deno.json

--- main.ts
+++ main.ts
@@ -1,14 +1,18 @@
 const DEPLOY_URL = Deno.env.get("DEPLOY_URL");
 
-Deno.cron("Vervel daily deploy message", "00 00 * * 2-6", async () => {
-  if (DEPLOY_URL) {
-    const response = await fetch(DEPLOY_URL);
-    if (!response.ok) {
-      console.error("Failed to send daily report:", response.statusText);
+function main() {
+  Deno.cron("Vervel daily deploy message", "23 59 * * 2-6", async () => {
+    if (DEPLOY_URL) {
+      const response = await fetch(DEPLOY_URL);
+      if (!response.ok) {
+        console.error("Failed to send daily report:", response.statusText);
+      } else {
+        console.log("Daily report sent successfully.");
+      }
     } else {
-      console.log("Daily report sent successfully.");
+      console.error("DEPLOY_URL is not set.");
     }
-  } else {
-    console.error("DEPLOY_URL is not set.");
-  }
-});
+  });
+}
+
+main();
